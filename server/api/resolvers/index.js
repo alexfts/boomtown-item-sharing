5c110a63c6fbf3f95d2e8d50ae452d79367feb58
alexfts <alxfetisova@gmail.com>
2019-01-17 11:24:18 -0800

    Connected Postgres and Apollo

---
+++ server/api/resolvers/index.js
@@ -0,0 +1,163 @@
+/**
+ *  @TODO: Handling Server Errors
+ *
+ *  Once you've completed your pg-resource.js methods and handled errors
+ *  use the ApolloError constructor to capture and return errors from your resolvers.
+ *
+ *  Throwing ApolloErrors from your resolvers is a nice pattern to follow and
+ *  will help you easily debug problems in your resolving functions.
+ *
+ *  It will also help you control th error output of your resource methods and use error
+ *  messages on the client! (More on that later).
+ *
+ *  The user resolver has been completed as an example of what you'll need to do.
+ *  Finish of the rest of the resolvers when you're ready.
+ */
+const { ApolloError } = require('apollo-server-express');
+
+// @TODO: Uncomment these lines later when we add auth
+// const jwt = require("jsonwebtoken")
+// const authMutations = require("./auth")
+// -------------------------------
+const { UploadScalar, DateScalar } = require('../custom-types');
+
+module.exports = app => {
+  return {
+    // Upload: UploadScalar,
+    // Date: DateScalar,
+
+    Query: {
+      viewer() {
+        /**
+         * @TODO: Authentication - Server
+         *
+         *  If you're here, you have successfully completed the sign-up and login resolvers
+         *  and have added the JWT from the HTTP cookie to your resolver's context.
+         *
+         *  The viewer is what we're calling the current user signed into your application.
+         *  When the user signed in with their username and password, an JWT was created with
+         *  the user's information cryptographically encoded inside.
+         *
+         *  To provide information about the user's session to the app, decode and return
+         *  the token's stored user here. If there is no token, the user has signed out,
+         *  in which case you'll return null
+         */
+        return null;
+      },
+      async user(parent, { id }, { pgResource }, info) {
+        try {
+          const user = await pgResource.getUserById(id);
+          return user;
+        } catch (e) {
+          throw new ApolloError(e);
+        }
+      },
+      async items(parent, { filter }, { pgResource }) {
+        try {
+          const items = await pgResource.getItems(filter);
+          return items;
+        } catch (e) {
+          throw new ApolloError(e);
+        }
+      },
+      async tags(parent, args, { pgResource }) {
+        try {
+          const tags = await pgResource.getTags();
+          return tags;
+        } catch (e) {
+          throw new ApolloError(e);
+        }
+      }
+    },
+
+    User: {
+      async items(user, _, { pgResource }) {
+        try {
+          const userItems = await pgResource.getItemsForUser(user.id);
+          return userItems;
+        } catch (e) {
+          throw new ApolloError(e);
+        }
+      },
+      async borrowed(user, _, { pgResource }) {
+        try {
+          const borrowedItems = await pgResource.getBorrowedItemsForUser(
+            user.id
+          );
+          return borrowedItems;
+        } catch (e) {
+          throw new ApolloError(e);
+        }
+      }
+    },
+
+    Item: {
+      async itemowner(item, _, { pgResource }) {
+        try {
+          const owner = await pgResource.getUserById(item.ownerid);
+          return owner;
+        } catch (e) {
+          throw new ApolloError(e);
+        }
+      },
+      async tags(item, _, { pgResource }) {
+        try {
+          const tags = await pgResource.getTagsForItem(item.id);
+          return tags;
+        } catch (e) {
+          throw new ApolloError(e);
+        }
+      },
+      async borrower(item, _, { pgResource }) {
+        if (!item.borrowerid) return null;
+        try {
+          const borrower = await pgResource.getUserById(item.borrowerid);
+          return borrower;
+        } catch (e) {
+          throw new ApolloError(e);
+        }
+      },
+      async imageurl({ imageurl, imageid, mimetype, data }) {
+        if (imageurl) return imageurl;
+        if (imageid) {
+          return `data:${mimetype};base64, ${data}`;
+        }
+      }
+    },
+
+    Mutation: {
+      // @TODO: Uncomment this later when we add auth
+      // ...authMutations(app),
+      // -------------------------------
+
+      async addItem(parent, args, context, info) {
+        /**
+         *  @TODO: Destructuring
+         *
+         *  The 'args' and 'context' parameters of this resolver can be destructured
+         *  to make things more readable and avoid duplication.
+         *
+         *  When you're finished with this resolver, destructure all necessary
+         *  parameters in all of your resolver functions.
+         *
+         *  Again, you may look at the user resolver for an example of what
+         *  destructuring should look like.
+         */
+
+        // image = await image;
+        // const user = await jwt.decode(context.token, app.get('JWT_SECRET'));
+        console.log(args);
+        try {
+          const newItem = await context.pgResource.saveNewItem({
+            item: args.item
+            //image: args.image,
+            //user
+          });
+          return newItem;
+        } catch (e) {
+          throw new ApolloError(e);
+        }
+      }
+    }
+  };
+};
---
+++ server/config/postgres.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg');
+
+module.exports = app => {
+  const host = app.get('PG_HOST');
+  const user = app.get('PG_USER');
+  const password = app.get('PG_PASSWORD');
+  const database = app.get('PG_DB');
+  return new Pool({
+    host,
+    user,
+    password,
+    database,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000
+  });
+};
