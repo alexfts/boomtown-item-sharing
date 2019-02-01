444d19cb75bbaad71758c39f55adcd8dbf84a2dd
alexfts <alxfetisova@gmail.com>
2019-02-01 12:07:08 -0800

    Improved screen loader UI

--- client/src/components/FullScreenLoader/FullScreenLoader.js
+++ client/src/components/FullScreenLoader/FullScreenLoader.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { CircularProgress, Typography } from '@material-ui/core';
+import styles from './styles';
+import { withStyles } from '@material-ui/core/styles';
 
 const FullScreenLoader = ({ classes }) => (
-  <div>
-    <Typography color="yellow">
-      "For it is in giving that we receive."
-    </Typography>
-    <CircularProgress color="yellow" />
+  <div className={classes.container} background-color="secondary">
+    <div className={classes.loader}>
+      <Typography color="primary" variant="headline">
+        "For it is in giving that we receive."
+      </Typography>
+      <CircularProgress color="primary" className={classes.spinner} />
+    </div>
   </div>
 );
 
-export default FullScreenLoader;
+export default withStyles(styles)(FullScreenLoader);
---
+++ client/src/components/FullScreenLoader/styles.js
@@ -0,0 +1,21 @@
+const styles = {
+  container: {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  loader: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  spinner: {
+    width: '50px',
+    height: '50px',
+    marginTop: '25px'
+  }
+};
+
+export default styles;
